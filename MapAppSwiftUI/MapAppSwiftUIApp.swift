452bea0b3b865e3367a89ef434b5455f3563496c
aslamdodeen <aslamdodeen@gmail.com>
2023-11-19 15:18:16 +0300

    Initial Commit

---
+++ MapAppSwiftUI/MapAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppSwiftUIApp.swift
+//  MapAppSwiftUI
+//
+//  Created by Islam Aldarabea on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
